5ba01b6c7ef3be4e6e02ee270d2edf7f01300d00
iamasianman <kevinle1991@gmail.com>
2019-05-27 19:18:10 -0700

    Comments module completed

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from "axios";
 import User from "./User.jsx";
-import OnHover from "./OnHover.jsx";
-
+import CommentsUser from "./CommentsUser.jsx";
+import HelpfulBtn from "./HelpfulBtn.jsx";
+import user from "../../../exampleData.js";
 class Comments extends React.Component {
   constructor(props) {
     super(props);
@@ -17,24 +18,40 @@ class Comments extends React.Component {
   getReviews() {
     axios
       .get("/review_id")
-      .then(data => {
-        this.setState({ repos: data });
-      })
+      .then(
+        data => {
+          console.log("here is the data", data["data"]);
+          this.setState({ repos: data["data"] });
+        },
+        data => {
+          console.log("data", data["data"]);
+        }
+      )
       .catch(err => {
         console.log(err);
       });
   }
 
   render() {
-    return (
-      <div id="container">
-        <User />
-        <OnHover />
-      </div>
+    console.log(
+      typeof this.state.repos,
+      Array.isArray(this.state.repos.data),
+      Object.keys(this.state.repos)
     );
-
-    // userpf section
-    //onHoover section
+    var items = this.state.repos.map(i => (
+      <div className="container">
+        <div className="profileSection">
+          <User data={i} />
+        </div>
+        <div className="commentsSection">
+          <CommentsUser data={i} />
+        </div>
+        <div className="helpfulButtonSection">
+          <HelpfulBtn />
+        </div>
+      </div>
+    ));
+    return <div>{items}</div>;
     // comments/reviews
     // pictures url
 
