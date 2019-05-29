8c22e16184706a66735d15c3a2f4dd67980b2c15
iamasianman <kevinle1991@gmail.com>
2019-05-28 19:16:51 -0700

    implemented Stars rating

--- client/src/components/CommentsUser.jsx
+++ client/src/components/CommentsUser.jsx
@@ -4,16 +4,24 @@ class CommentsUser extends React.Component {
     super();
   }
   render() {
+    let userStars;
+    if (this.props.data.stars === 1) {
+      userStars = "oneStar";
+    } else if (this.props.data.stars === 2) {
+      userStars = "twoStar";
+    } else if (this.props.data.stars === 3) {
+      userStars = "threeStar";
+    } else if (this.props.data.stars === 4) {
+      userStars = "fourStar";
+    } else {
+      userStars = "fiveStar ";
+    }
     return (
       <div className="reviewSection">
-        <span>
-          <img
-            className="starsIcon"
-            src="https://dealercarsearch.com/DealerImages/9828/Yelp%205%20stars.png"
-          />
-        </span>
-        <span className="dateWritten">{this.props.data.date}</span>
-        {/* comments */}
+        <div className="starandtime">
+          <div className={userStars} />
+          <div className="dateWritten">{this.props.data.date}</div>
+        </div>
         <p className="userText">{this.props.data.comments}</p>
       </div>
     );
